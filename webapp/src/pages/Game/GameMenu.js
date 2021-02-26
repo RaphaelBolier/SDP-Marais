952c4575433ec430b22b9b437308a1d2127f68c4
LGuillaumet <leo.guillaumet@etu.unice.fr>
2021-02-26 22:05:33 +0100

    #11 GameMenu page

---
+++ webapp/src/pages/Game/GameMenu.js
@@ -0,0 +1,18 @@
+import {
+	Container,
+} from 'reactstrap';
+import './GameMenu.scss';
+
+const GameMenu = () => {
+
+	return (
+		<Container className="GameMenu">
+			<h1 className="text-center">Game</h1>
+			<Container className='text-center h-50 Home-container align-middle'>
+
+			</Container>
+		</Container>
+	);
+};
+
+export default GameMenu;
