16267ed7c8d95ef3ac55ae46d09069fce0599950
LGuillaumet <leo.guillaumet@etu.unice.fr>
2021-02-27 23:12:27 +0100

    #5 Première ébauche des règles

--- webapp/src/pages/Rules/Rules.js
+++ webapp/src/pages/Rules/Rules.js
@@ -8,11 +8,85 @@ const GameMenu = () => {
 	return (
 		<Container className='text-center h-50 Home-container align-middle'>
 			<Card>
-				<CardHeader>Régles</CardHeader>
+				<CardHeader>
+					<h1>Régles de Who did it?!</h1>
+				</CardHeader>
 				<CardBody className='text-left'>
-					<p>1:</p>
-					<p>2:</p>
-					<p>3:</p>
+					<h2>But du jeu </h2>
+					<p> Le jeu est très simple : il y a des survivants, appelés  « Innocent »,
+					et des tueurs, appelés « imposteurs ».
+					Le but des innocents est de survivre tout en faisant des missions éparpillées sur la carte.
+					Quant aux imposteurs, ils doivent essayer de tuer tous les innocents.</p>
+					<p>La partie est découpée en plusieurs phases :</p>
+					<ul>
+						<li>
+							<b>Missions : </b>
+							<span>
+								Au début de cette phase les joueurs apparaissent sur la carte et son libres de se déplacer librement, des "missions"
+								sont attribuées aux innocents, l'objectif étant d'effectuer le plus de missions possibles.
+								Les imposteurs devront alors se faire passer pour un joueur innocent tout en éliminant les différents innocents qu'il croisera.
+							</span>
+						</li>
+						<li>
+							<b>Interrogatoire : </b>
+							<span>
+								Si un joueur découvre la dépouille d'un autre joueur ou s'il a demandé une réunion d'urgence le jeu passe en phase d'interrogatoire.
+								Les joueurs vont alors débattre entre eux afin de trouver les imposteurs.
+								Le joueur ayant le plus de vote à l'issu du débat sera elliminé.
+								Les imposteurs devront alors gagner la confiance des autres joueurs afin de ne pas se faire éliminer à l'issu du vote.
+						</span>
+						</li>
+					</ul>
+					<h5>Condition de victoire Innocent : </h5>
+					<span>Si toutes les missions ont été effectuées ou s'il ne reste plus d'imposteurs les innocents gagnent la partie.</span>
+					<h5>Condition de victoire Imposteur : </h5>
+					<span>Si le nombre d'imposteurs est supérieur au nombre d'innocent le ou les imposteurs remportent la partie.</span>
+					<h2>Rôles :</h2>
+					<ul>
+						<li>Imposteur :</li>
+						<ul>
+							<li>
+								Il a une meilleure vision, il  peut donc voir dans le noir et plus loin que les joueurs <b>Innocents</b>.
+							</li>
+							<li>
+								Il peut éliminer les joueurs <b>Innocents</b>.
+							</li>
+							<li>
+								Il ne peut pas effectuer de <b>Missions</b>.
+							</li>
+							<li>
+								Il peut déclencher des <b>Urgences</b> mais ne pas les résoudres.
+							</li>
+							<li>
+								Il peut interragir avec certains des éléments de la carte (Raccourcis, cachette, porte...).
+							</li>
+						</ul>
+						<li>Innocent :</li>
+						<ul>
+							<li>
+								Il a une vision basique, il a donc du mal à voir dans le noir.
+							</li>
+							<li>
+								Il ne peut effectuer des <b>Missions</b>.
+							</li>
+							<li>
+								Il peut résoudre des <b>Urgences</b>.
+							</li>
+							<li>
+								Il ne peut pas interragir avec certains des éléments de la carte (Raccourcis, cachette, porte...).
+							</li>
+						</ul>
+					</ul>
+					<h2>Missions :</h2>
+						<p>Tâches que doivent effectuer les joueurs <b>Innocents</b>.</p>
+						<p>Elles peuvent s'apprenter à des énigmes ou un enchainement d'actions simples.</p>
+					<h2>Urgences :</h2>
+					<p>Tâches déclenchées par les joueurs <b>Imposteurs</b>.</p>
+					<p>Elles peuvent s'apprenter à des énigmes ou un enchainement d'actions simples.</p>
+					<p>Selon l'urgence, la tâche doit être résolue dans une limite de temps sinon cela pourrait entrainer la mort des joueurs innocents</p>
+
+
+
 				</CardBody>
 			</Card>
 
