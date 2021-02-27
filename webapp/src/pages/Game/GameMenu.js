88935f13fbcb6287a796ff110a725cfe98f3ccb9
LGuillaumet <leo.guillaumet@etu.unice.fr>
2021-02-27 23:43:33 +0100

    #7 Le joueur peut entrer un pseudo

--- webapp/src/pages/Game/GameMenu.js
+++ webapp/src/pages/Game/GameMenu.js
@@ -1,14 +1,43 @@
+import { useState } from 'react';
 import {
-	Container,
+	Container, Card, CardHeader, CardBody,
 } from 'reactstrap';
 import './GameMenu.scss';
 
 const GameMenu = () => {
 
+	const [playerName, setPlayerName] = useState('');
+	console.log(playerName);
+
+
+	const handlePlayerName = (event) => {
+		setPlayerName(event.target.value);
+		console.log(playerName);
+
+	}
+
+	const handleSubmit = (event) => {
+		alert('Le nom a été soumis : ' + playerName);
+		event.preventDefault();
+	  }
 	return (
 		<Container className="GameMenu">
 			<h1 className="text-center">Game</h1>
 			<Container className='text-center h-50 Home-container align-middle'>
+				<Card>
+					<CardHeader>
+						<form onSubmit={handleSubmit}>
+							<label>
+								Pseudo : <input type="text" value={playerName} onChange={handlePlayerName} />
+							</label>
+							<br/>
+							<input type="submit" value="Valider" />
+						</form>
+					</CardHeader>
+					<CardBody>
+
+					</CardBody>
+				</Card>
 
 			</Container>
 		</Container>
