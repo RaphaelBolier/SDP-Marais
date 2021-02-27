5310b412fb1aec7b12cfb909648d374197f42447
Dorian <dorian.chapoulie@gmail.com>
2021-02-27 17:44:41 +0100

    ajout de la gestion des body pour les requêtes post #30

--- api/src/server.js
+++ api/src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+
 const game = require('./routes/game');
 
 const port = 3000;
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 app.use('/game', game);
 
