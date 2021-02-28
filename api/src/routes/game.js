8b36782855b53dc9490ecba15e2e2386db9c0c5f
Dorian <dorian.chapoulie@gmail.com>
2021-02-28 21:21:41 +0100

    Serveur : Ajout de la route rejoindre une partie #19

--- api/src/routes/game.js
+++ api/src/routes/game.js
@@ -1,10 +1,31 @@
 const { Router } = require('express');
-const { getGames, createGame } = require('../services/game');
+const { getGames, createGame, joinGame } = require('../services/game');
 const { joinRoom } = require('../wsapi');
 
 const router = Router();
 
 router.get('/', (req, res) => res.send(JSON.stringify(getGames())));
+
+router.post('/:id', (req, res) => {
+    const { id } = req.params;
+    const { clientId } = req.body;
+
+    const game = getGames().find((game) => game.id === id);
+
+    if (game) {
+        joinRoom(clientId, id);
+        joinGame(clientId, id);
+        res.send({ gameId: id });
+        console.log(`Client ${clientId} joined game ${id}`);
+    } else {
+        res.send({
+            error: true,
+            msg: 'Failed to join the game.',
+            gameId: id,
+            clientId,
+        });
+    }
+});
 
 router.post('/', (req, res) => {
     const { gameName, id } = req.body;
--- api/src/services/game.js
+++ api/src/services/game.js
@@ -4,5 +4,14 @@ const games = [];
 exports.getGames = () => games;
 
 exports.createGame = (name, id) => {
-    games.push({ name, id });
+    games.push({ name, id, players: [id] });
+}
+
+exports.joinGame = (clientId, gameId) => {
+    const game = games.find((game) => game.id === gameId);
+    if (game) {
+        game.players.push(clientId);
+    } else {
+        throw new Error('Cannot find game: ', gameId);
+    }
 }
