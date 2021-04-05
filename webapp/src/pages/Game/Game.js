d1ce44d19426fc330f5dd0b77332850f4d5a80a8
LGuillaumet <leo.guillaumet@etu.unice.fr>
2021-04-05 21:24:50 +0200

    #66 better design lobby

--- webapp/src/pages/Game/Game.js
+++ webapp/src/pages/Game/Game.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState, } from 'react';
 import { useParams } from "react-router-dom";
-import { Button } from 'reactstrap';
+import { Button, Col, Container, Row } from 'reactstrap';
 
 import { initInputsEvent, moveEntity, checkColision } from '../../lib/Input';
 import { Player } from '../../lib/Entity/Player';
@@ -11,10 +11,10 @@ import { useAudios } from '../../components/Audio/AudioProvider';
 import { getClosestEntity } from '../../lib/Utils';
 import mapLobby from '../../assets/map/lobby/lobby.json';
 import { tasks } from '../../components/Task';
-import PopOverContainer  from '../../components/Popover/PopOverContainer';
+import PopOverContainer from '../../components/Popover/PopOverContainer';
 import ModalContainer from '../../components/Modal/ModalContainer';
 
-import './GameCreate.scss';
+import './Game.scss';
 import { Direction } from '../../lib/Entity/Entity';
 
 const MAX_KILL_DIST = 100;
@@ -38,6 +38,7 @@ const GameMenu = () => {
     const [isDead, setIsDead] = useState(false);
     const [isKillButtonEnabled, setKillButton] = useState(false);
     const [CurrentTask, setCurrentTask] = useState(undefined);
+    const [taskProgression, setTaskProgression] = useState(0);
 
     const handleClickKill = () => {
         localPlayer.startKillCoolDown(KILL_COOLDOWN);
@@ -51,7 +52,7 @@ const GameMenu = () => {
         if (!canvasRef.current) return;
         initInputsEvent();
 
-        setInterval(() => {            
+        setInterval(() => {
             if (localPlayer.isImpostor
                 && players.length
                 && getClosestEntity(localPlayer, players).distance <= MAX_KILL_DIST
@@ -101,7 +102,7 @@ const GameMenu = () => {
             });
             socket.on('playerposition', ({ id, x, y, direction }) => {
                 const target = players.find((player) => player.id === id);
-                if (target) { 
+                if (target) {
                     if (localPlayer.isDead) {
                         target.direction = direction;
                         target.x = x;
@@ -111,20 +112,20 @@ const GameMenu = () => {
                         target.x = x;
                         target.y = y;
                     }
-                   
+
                 }
-            }); 
+            });
             socket.on('playerkilled', ({ targetId }) => {
                 const target = players.find((player) => player.id === targetId);
-                if (target) { 
+                if (target) {
                     target.kill();
                 } else if (localPlayer.id === targetId) {
                     localPlayer.kill();
                     setIsDead(true);
                 }
-            });    
+            });
         }
-        
+
         const draw = (context) => {
             drawMap(context, mapLobby.tiles, LOBBY_WIDTH, TILE_SIZE);
             localPlayer.draw();
@@ -139,13 +140,13 @@ const GameMenu = () => {
 
             if (collisionTasks.length > 0) {
                 if (!CurrentTask) {
-                    for(const task of collisionTasks) {
+                    for (const task of collisionTasks) {
                         const isTaskAlreadyFinished = finishedTasks.find((t) => t.id === task.id);
                         if (!isTaskAlreadyFinished) {
-                            setCurrentTask(task);   
+                            setCurrentTask(task);
                             break;
                         }
-                    }         
+                    }
                 }
             } else {
                 setCurrentTask(undefined);
@@ -157,28 +158,62 @@ const GameMenu = () => {
         initMap();
         getPlayerList(id);
         playAudio(audioIds.JOIN);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    useEffect(() => {
+        if(isNaN(finishedTasks.length)){
+            setTaskProgression(0);
+        }
+        else{
+        setTaskProgression(finishedTasks.length / taskTiles.length * 100);
+        }
+    }, [CurrentTask]);
+
+
     return (
-        <div>
-            {CurrentTask && (
-                // eslint-disable-next-line react/jsx-pascal-case
-                <CurrentTask.component
-                    localPlayer={localPlayer}
-                    task={CurrentTask}
-                    notifyEnd={() => finishedTasks.push(CurrentTask)}
-                />
-            )}
-            <canvas ref={canvasRef} id="canvas" width="576" height="576" />
-            <p> partie: {id} </p>
-            { isImpostor && (
-                <Button onClick={handleClickKill} disabled={!isKillButtonEnabled}>
-                    KILL
-                </Button>
-            )}
-            <PopOverContainer/>
-            <ModalContainer openBool={isDead} toggleModal={() => setIsDead(!isDead)}/>
+        <div className="GameCanvas Lobby">
+            <Container>
+                <Row className="d-flex justify-content-center my-2">
+                    <Col xs="5">
+                        <div className="progress">
+                            <div className={`progress-bar bg-success`}
+                                style={{ width: taskProgression + '%'}}
+                                role="progressbar" aria-valuenow={taskProgression}
+                                aria-valuemin="0"
+                                aria-valuemax="100">
+                                {taskProgression}%
+                                </div>
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>Partie: {id} </p>
+                        <p>Nombre de joueurs : {1 + players.length} </p>
+                    </Col>
+                    <Col>
+                        {CurrentTask && (
+                            // eslint-disable-next-line react/jsx-pascal-case
+                            <CurrentTask.component
+                                localPlayer={localPlayer}
+                                task={CurrentTask}
+                                notifyEnd={() => finishedTasks.push(CurrentTask)}
+                            />
+                        )}
+                        <canvas ref={canvasRef} id="canvas" width="576" height="576" className="mx-auto d-flex" />
+                    </Col>
+                    <Col>
+                        {isImpostor && (
+                            <Button onClick={handleClickKill} disabled={!isKillButtonEnabled}>
+                                KILL
+                            </Button>
+                        )}
+                        <PopOverContainer />
+                    </Col>
+                </Row>
+            </Container>
+            <ModalContainer openBool={isDead} toggleModal={() => setIsDead(!isDead)} />
         </div>
     );
 };
