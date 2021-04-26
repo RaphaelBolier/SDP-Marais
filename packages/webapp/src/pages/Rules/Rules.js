41c799bf373d9dbd45f6c82a689caf772fe3856d
HoudaBs <boustati.houda@outlook.fr>
2021-04-26 18:55:07 +0200

    update rules

--- packages/webapp/src/pages/Rules/Rules.js
+++ packages/webapp/src/pages/Rules/Rules.js
@@ -14,8 +14,8 @@ const GameMenu = () => {
 				</CardHeader>
 				<CardBody className='text-left'>
 					<h2>But du jeu </h2>
-					<p> Le jeu est très simple : il y a des survivant appelés  « Innocents »
-					et des tueur appelés « imposteurs ».
+					<p> Le jeu est très simple : il y a des survivants appelés  « Innocents »
+					et des tueurs appelés « imposteurs ».
 					Le but des innocents est de survivre tout en faisant des missions éparpillées sur la carte.
 					Quant aux imposteurs, ils doivent essayer de tuer tous les innocents.</p>
 					<p>La partie est découpée en plusieurs phases :</p>
@@ -31,15 +31,15 @@ const GameMenu = () => {
 						<li>
 							<b>Interrogatoire : </b>
 							<span>
-								Si un joueur découvre la dépouille d'un autre joueur ou s'il a demandé une réunion d'urgence le jeu passe en phase d'interrogatoire.
+								Si un joueur découvre la dépouille d'un autre joueur le jeu passe en phase d'interrogatoire.
 								Les joueurs vont alors débattre entre eux afin de trouver les imposteurs.
 								Le joueur ayant le plus de vote à l'issu du débat sera elliminé.
 								Les imposteurs devront alors gagner la confiance des autres joueurs afin de ne pas se faire éliminer à l'issu du vote.
 						</span>
 						</li>
 					</ul>
-					<h5>Condition de victoire Innocent : </h5>
-					<span>Si toutes les missions ont été effectuées ou s'il ne reste plus d'imposteurs les innocents gagnent la partie.</span>
+					<h5>Conditions de victoire Innocent : </h5>
+					<span>Si les innocents ont trouvé le tueur ou si la partie est arrivée à son terme et qu'il reste plus d'imposteurs, les innocents gagnent la partie.</span>
 					<h5>Condition de victoire Imposteur : </h5>
 					<span>Si le nombre d'imposteurs est supérieur au nombre d'innocent le ou les imposteurs remportent la partie.</span>
 					<h2>Roles :</h2>
@@ -53,13 +53,7 @@ const GameMenu = () => {
 								Il peut éliminer les joueurs <b>Innocents</b>.
 							</li>
 							<li>
-								Il ne peut pas effectuer de <b>Missions</b>.
-							</li>
-							<li>
-								Il peut déclencher des <b>Urgences</b> mais ne pas les résoudres.
-							</li>
-							<li>
-								Il peut interragir avec certains des éléments de la carte (Raccourcis, cachette, porte...).
+								Il peut rallumer la <b>lumière</b>.
 							</li>
 						</ul>
 						<li>Innocent :</li>
@@ -68,23 +62,13 @@ const GameMenu = () => {
 								Il a une vision basique, il a donc du mal à voir dans le noir.
 							</li>
 							<li>
-								Il peut effectuer des <b>Missions</b>.
-							</li>
-							<li>
-								Il peut résoudre des <b>Urgences</b>.
-							</li>
-							<li>
-								Il ne peut pas interagir avec certains des éléments de la carte (Raccourcis, cachette, porte...).
+								Il peut rallumer la <b>lumière</b>.
 							</li>
 						</ul>
 					</ul>
-					<h2>Missions :</h2>
-						<p>Tâches que doivent effectuer les joueurs <b>Innocents</b>.</p>
-						<p>Elles peuvent s'apparenter à des énigmes ou un enchainement d'actions simples.</p>
-					<h2>Urgences :</h2>
-					<p>Tâches déclenchées par les joueurs <b>Imposteurs</b>.</p>
-					<p>Elles peuvent s'apparenter à des énigmes ou un enchainement d'actions simples.</p>
-					<p>Selon l'urgence, la tâche doit être résolue dans une limite de temps sinon cela pourrait entrainer la mort des joueurs innocents</p>
+					<h2>Evenements  :</h2>
+						<p>L'Imposteur peut éteindre la <b>lumière</b>.</p>
+						<p>L'innocent doit faire une mission pour rallumer la <b>lumière</b>. </p>
 				</CardBody>
 			</Card>
 		</Container >
